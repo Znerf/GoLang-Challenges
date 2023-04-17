5ab9f2fb1932a001ab806e4f60e363c6ac1d0ef8
Sagar Shrestha <znerfii.a1@gmail.com>
2023-04-17 12:42:29 +0545

    Update 1. Test for string Handling.go

--- 1. Test for string Handling.go
+++ 1. Test for string Handling.go
@@ -10,7 +10,7 @@ func solution(str string, ending string) bool {
   return a == ending
   
 
-////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 /*Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
 Examples:
